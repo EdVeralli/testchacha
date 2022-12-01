48223db81e03a4bf673f0fbe36b40721110f9661
Eduardo Damian Veralli <edveralli@gmail.com>
2022-12-01 10:52:59 -0300

    cipher in function

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 use aead::{AeadInPlace, Key, NewAead, Nonce};
 use xchacha8blake3siv::XChaCha8Blake3Siv;
+use std::str;
 
 fn main() {
+    let tag_01:GenericArray = chachablake_cipher(b"an example very very secret key.",b"extra long unique nonce!",b"plaintext message" );
+}
 
-    // texto para hacer el cifrado
-    let key = Key::<XChaCha8Blake3Siv>::from_slice(b"an example very very secret key."); // 32-bytes
-    // hace el cifrado usando el texto de arriba
-    let cipher = XChaCha8Blake3Siv::new(key);
-    // crea el nonce basado en un texto
-    let nonce = Nonce::<XChaCha8Blake3Siv>::from_slice(b"extra long unique nonce!"); // 24-bytes; unique per message
-    // texto util a cifrar
-    let mut buffer = b"plaintext message".to_owned();
 
-    //"associated data" lo usa para hacer distinto el largo del plain text.
+fn chachablake_cipher(text_key: &'static [u8; 32], text_nonce: &'static [u8; 32], text_assoc: &'static [u8; 32]) -> GenericArray<u8, UInt<UInt<UInt<UInt<UInt<UInt<UTerm, B1>, B0>, B0>  {
 
-    let tag = cipher.encrypt_in_place_detached(nonce, b"associated data", &mut buffer)
-        .expect("encryption failure!");  // NOTE: handle this error to avoid panics!
-        
-    cipher.decrypt_in_place_detached(nonce, b"associated data", &mut buffer, &tag)
-        .expect("decryption failure!");  // NOTE: handle this error to avoid panics!
+        // texto para hacer el cifrado
+        let key = Key::<XChaCha8Blake3Siv>::from_slice(text_key); // 32-bytes
+        // hace el cifrado usando el texto de arriba
+        let cipher = XChaCha8Blake3Siv::new(key);
+        // crea el nonce basado en un texto
+        let nonce = Nonce::<XChaCha8Blake3Siv>::from_slice(text_nonce); // 24-bytes; unique per message
+        // texto util a cifrar
+        let mut buffer = b"plaintext message".to_owned();
     
-    assert_eq!(&buffer, b"plaintext message");
+        //"associated data" lo usa para hacer distinto el largo del plain text.
+    
+        let tag = cipher.encrypt_in_place_detached(nonce, b"associated data", &mut buffer)
+            .expect("encryption failure!");  // NOTE: handle this error to avoid panics!
+            
+        cipher.decrypt_in_place_detached(nonce, b"associated data", &mut buffer, &tag)
+            .expect("decryption failure!");  // NOTE: handle this error to avoid panics!
+        
+        assert_eq!(&buffer, b"plaintext message");
+  
+        tag
+
 }
