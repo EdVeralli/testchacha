f7e6c5920283fe70d744f688b1d54b562a499830
Eduardo Damian Veralli <edveralli@gmail.com>
2022-12-02 15:27:49 -0300

    Deymo Fixed errors in Params

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
 use aead::{AeadInPlace, Key, NewAead, Nonce};
+use std::ops::Deref;
 use xchacha8blake3siv::XChaCha8Blake3Siv;
-use std::str;
 
 fn main() {
-    let tag_01:GenericArray = chachablake_cipher(b"an example very very secret key.",b"extra long unique nonce!",b"plaintext message" );
+    let mut buffer = b"plaintext message".to_owned();
+
+    let _tag_01 = encrypt(
+        b"an example very very secret key.",
+        b"extra long unique nonce!",
+        b"asoc data",
+        &mut buffer,
+    );
 }
 
+fn encrypt(
+    text_key: &[u8; 32],
+    text_nonce: &[u8; 24],
+    associated_data: &[u8],
+    buffer: &mut [u8],
+) -> Vec<u8> {
+    // texto para hacer el cifrado
+    let key = Key::<XChaCha8Blake3Siv>::from_slice(text_key); // 32-bytes
+                                                              // hace el cifrado usando el texto de arriba
+    let cipher = XChaCha8Blake3Siv::new(key);
+    // crea el nonce basado en un texto
+    let nonce = Nonce::<XChaCha8Blake3Siv>::from_slice(text_nonce); // 24-bytes; unique per message
+                                                                    // texto util a cifrar
+                                                                    //"associated data" lo usa para hacer distinto el largo del plain text.
+    let tag = cipher
+        .encrypt_in_place_detached(nonce, associated_data, buffer)
+        .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
 
-fn chachablake_cipher(text_key: &'static [u8; 32], text_nonce: &'static [u8; 32], text_assoc: &'static [u8; 32]) -> GenericArray<u8, UInt<UInt<UInt<UInt<UInt<UInt<UTerm, B1>, B0>, B0>  {
+    Vec::<u8>::from(tag.deref())
+}
 
-        // texto para hacer el cifrado
-        let key = Key::<XChaCha8Blake3Siv>::from_slice(text_key); // 32-bytes
-        // hace el cifrado usando el texto de arriba
-        let cipher = XChaCha8Blake3Siv::new(key);
-        // crea el nonce basado en un texto
-        let nonce = Nonce::<XChaCha8Blake3Siv>::from_slice(text_nonce); // 24-bytes; unique per message
-        // texto util a cifrar
-        let mut buffer = b"plaintext message".to_owned();
-    
-        //"associated data" lo usa para hacer distinto el largo del plain text.
-    
-        let tag = cipher.encrypt_in_place_detached(nonce, b"associated data", &mut buffer)
-            .expect("encryption failure!");  // NOTE: handle this error to avoid panics!
-            
-        cipher.decrypt_in_place_detached(nonce, b"associated data", &mut buffer, &tag)
-            .expect("decryption failure!");  // NOTE: handle this error to avoid panics!
-        
-        assert_eq!(&buffer, b"plaintext message");
-  
-        tag
+/*
+fn decrypt(
+    text_key: &[u8; 32],
+    text_nonce: &[u8; 24],
+    associated_data: &[u8],
+) -> Vec<u8> {
+
+    cipher
+        .decrypt_in_place_detached(nonce, associated_data, &mut buffer, &tag)
+        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+
+    assert_eq!(&buffer, b"plaintext message");
 
 }
+*/
